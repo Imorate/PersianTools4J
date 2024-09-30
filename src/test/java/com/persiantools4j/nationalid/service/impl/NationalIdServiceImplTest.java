8ebe1bf226a3f79f43a7873291cec448d2917414
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-10-01 01:34:40 +0330

    Update test files and improve readability

--- src/test/java/com/persiantools4j/nationalid/service/impl/NationalIdServiceImplTest.java
+++ src/test/java/com/persiantools4j/nationalid/service/impl/NationalIdServiceImplTest.java
@@ -4,6 +4,7 @@ import com.persiantools4j.exception.ValidationException;
 import com.persiantools4j.nationalid.service.NationalIdService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -57,56 +58,72 @@ class NationalIdServiceImplTest {
         nationalIdService = NationalIdServiceImpl.getInstance();
     }
 
-    @Test
-    @DisplayName("Get instance test")
-    void testGetInstance() {
-        NationalIdService firstNationalIdService = NationalIdServiceImpl.getInstance();
-        assertThat(firstNationalIdService).isNotNull();
-        NationalIdService secondNationalIdService = NationalIdServiceImpl.getInstance();
-        assertThat(secondNationalIdService).isNotNull();
-        assertThat(firstNationalIdService).isSameAs(secondNationalIdService);
+    @Nested
+    @DisplayName("Is valid tests")
+    class IsValidTests {
+
+        @ParameterizedTest
+        @MethodSource("com.persiantools4j.nationalid.service.impl.NationalIdServiceImplTest#validNationalIdCases")
+        @DisplayName("Valid National ID test")
+        void testValidNationalId(String nationalId) {
+            assertThat(nationalIdService.isValid(nationalId)).isTrue();
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.persiantools4j.nationalid.service.impl.NationalIdServiceImplTest#invalidNationalIdCases")
+        @DisplayName("Invalid National ID test")
+        void testInvalidNationalId(String nationalId) {
+            assertThat(nationalIdService.isValid(nationalId)).isFalse();
+        }
+
     }
 
-    @Test
-    @DisplayName("Get instance thread-safe test")
-    void testGetInstanceThreadSafe() throws InterruptedException {
-        final NationalIdService[] nationalIdServices = new NationalIdService[2];
-        Thread firstThread = new Thread(() -> nationalIdServices[0] = NationalIdServiceImpl.getInstance());
-        Thread secondThread = new Thread(() -> nationalIdServices[1] = NationalIdServiceImpl.getInstance());
-        firstThread.start();
-        secondThread.start();
-        firstThread.join();
-        secondThread.join();
-        assertThat(nationalIdServices[0]).isNotNull();
-        assertThat(nationalIdServices[1]).isNotNull();
-        assertThat(nationalIdServices[0]).isSameAs(nationalIdServices[1]);
+    @Nested
+    @DisplayName("Validation tests")
+    class ValidationTests {
+
+        @ParameterizedTest
+        @MethodSource("com.persiantools4j.nationalid.service.impl.NationalIdServiceImplTest#validNationalIdCases")
+        @DisplayName("National ID validation test")
+        void testValidateNationalId(String nationalId) {
+            assertThatCode(() -> nationalIdService.validate(nationalId)).doesNotThrowAnyException();
+        }
+
+        @ParameterizedTest
+        @MethodSource("com.persiantools4j.nationalid.service.impl.NationalIdServiceImplTest#invalidNationalIdCases")
+        @DisplayName("Exceptional validation National ID test")
+        void testExceptionValidateNationalId(String nationalId) {
+            assertThatThrownBy(() -> nationalIdService.validate(nationalId)).isInstanceOf(ValidationException.class);
+        }
+
     }
 
-    @ParameterizedTest
-    @MethodSource("validNationalIdCases")
-    @DisplayName("Valid National ID test")
-    void testValidNationalId(String nationalId) {
-        assertThat(nationalIdService.isValid(nationalId)).isTrue();
-    }
+    @Nested
+    @DisplayName("Get instance tests")
+    class GetInstanceTests {
+        @Test
+        @DisplayName("Non-thread-safe test")
+        void testGetInstance() {
+            NationalIdService firstNationalIdService = NationalIdServiceImpl.getInstance();
+            assertThat(firstNationalIdService).isNotNull();
+            NationalIdService secondNationalIdService = NationalIdServiceImpl.getInstance();
+            assertThat(secondNationalIdService).isNotNull();
+            assertThat(firstNationalIdService).isSameAs(secondNationalIdService);
+        }
 
-    @ParameterizedTest
-    @MethodSource("invalidNationalIdCases")
-    @DisplayName("Invalid National ID test")
-    void testInvalidNationalId(String nationalId) {
-        assertThat(nationalIdService.isValid(nationalId)).isFalse();
-    }
-
-    @ParameterizedTest
-    @MethodSource("validNationalIdCases")
-    @DisplayName("National ID validation test")
-    void testValidateNationalId(String nationalId) {
-        assertThatCode(() -> nationalIdService.validate(nationalId)).doesNotThrowAnyException();
-    }
-
-    @ParameterizedTest
-    @MethodSource("invalidNationalIdCases")
-    @DisplayName("Exceptional validation National ID test")
-    void testExceptionValidateNationalId(String nationalId) {
-        assertThatThrownBy(() -> nationalIdService.validate(nationalId)).isInstanceOf(ValidationException.class);
+        @Test
+        @DisplayName("Thread-safe test")
+        void testGetInstanceThreadSafe() throws InterruptedException {
+            final NationalIdService[] nationalIdServices = new NationalIdService[2];
+            Thread firstThread = new Thread(() -> nationalIdServices[0] = NationalIdServiceImpl.getInstance());
+            Thread secondThread = new Thread(() -> nationalIdServices[1] = NationalIdServiceImpl.getInstance());
+            firstThread.start();
+            secondThread.start();
+            firstThread.join();
+            secondThread.join();
+            assertThat(nationalIdServices[0]).isNotNull();
+            assertThat(nationalIdServices[1]).isNotNull();
+            assertThat(nationalIdServices[0]).isSameAs(nationalIdServices[1]);
+        }
     }
 }
--- src/test/java/com/persiantools4j/utils/NumberUtilsTest.java
+++ src/test/java/com/persiantools4j/utils/NumberUtilsTest.java
@@ -1,6 +1,7 @@
 package com.persiantools4j.utils;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -10,17 +11,24 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 @DisplayName("Number utils")
 class NumberUtilsTest {
 
-    @ParameterizedTest(name = "[{index}] {0} {1}nth numeric character value is {2}")
-    @DisplayName("Get numeric value test")
-    @CsvSource({"1,0,1", "6104038932,9,2", "523,2,3"})
-    void getNumericValueTest(String str, int index, int expected) {
-        assertThat(NumberUtils.getNumericValue(str, index)).isEqualTo(expected);
+    @Nested
+    @DisplayName("Get Numeric Value Tests")
+    class GetNumericValueTests {
+
+        @ParameterizedTest(name = "[{index}] {0} {1}nth numeric character value is {2}")
+        @DisplayName("Valid Inputs")
+        @CsvSource({"1,0,1", "6104038932,9,2", "523,2,3"})
+        void getNumericValueTest(String str, int index, int expected) {
+            assertThat(NumberUtils.getNumericValue(str, index)).isEqualTo(expected);
+        }
+
+        @ParameterizedTest(name = "[{index}] {0} {1}nth numeric character value")
+        @DisplayName("Exceptional Inputs")
+        @CsvSource({"1,3", "6104038932,10", "523,6", "test,1"})
+        void getNumericValueExceptionalTest(String str, int index) {
+            assertThatThrownBy(() -> NumberUtils.getNumericValue(str, index)).isInstanceOf(IllegalArgumentException.class);
+        }
+
     }
 
-    @ParameterizedTest(name = "[{index}] {0} {1}nth numeric character value")
-    @DisplayName("Get numeric value exceptional test")
-    @CsvSource({"1,3", "6104038932,10", "523,6", "test,1"})
-    void getNumericValueExceptionalTest(String str, int index) {
-        assertThatThrownBy(() -> NumberUtils.getNumericValue(str, index)).isInstanceOf(IllegalArgumentException.class);
-    }
 }
