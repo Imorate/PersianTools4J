9aa9329ca40fddd7ccf255467adf86cd14496c31
Amir Mohammad Hl <dev.imorate@gmail.com>
2024-10-01 01:20:18 +0330

    Update NumberUtils and add test

--- src/main/java/com/persiantools4j/utils/NumberUtils.java
+++ src/main/java/com/persiantools4j/utils/NumberUtils.java
@@ -3,6 +3,9 @@ package com.persiantools4j.utils;
 public class NumberUtils {
 
     public static int getNumericValue(String input, int index) {
+        if (!input.matches("^\\d+$") || index >= input.length()) {
+            throw new IllegalArgumentException("Invalid number");
+        }
         return Character.getNumericValue(input.charAt(index));
     }
 
--- src/test/java/com/persiantools4j/utils/NumberUtilsTest.java
+++ src/test/java/com/persiantools4j/utils/NumberUtilsTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @DisplayName("Number utils")
 class NumberUtilsTest {
@@ -14,5 +15,12 @@ class NumberUtilsTest {
     @CsvSource({"1,0,1", "6104038932,9,2", "523,2,3"})
     void getNumericValueTest(String str, int index, int expected) {
         assertThat(NumberUtils.getNumericValue(str, index)).isEqualTo(expected);
+    }
+
+    @ParameterizedTest(name = "[{index}] {0} {1}nth numeric character value")
+    @DisplayName("Get numeric value exceptional test")
+    @CsvSource({"1,3", "6104038932,10", "523,6", "test,1"})
+    void getNumericValueExceptionalTest(String str, int index) {
+        assertThatThrownBy(() -> NumberUtils.getNumericValue(str, index)).isInstanceOf(IllegalArgumentException.class);
     }
 }
